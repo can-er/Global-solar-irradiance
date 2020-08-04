6487f72e0dce6d65242b1924ba1e398bd6b11551
Caner <2broglie@gmail.com>
2020-08-04 15:16:55 +0200

    Use of OOP
    The hardcoded program changed to OOP to be compatible with Django framework

--- rgj.py
+++ rgj.py
@@ -6,77 +6,64 @@ Date: 15-07-20
 Description: This program allows to obtain the daily global radiation of the sun.
 """
 
-"""
-Hesite pas à intégrer des tas de print() pour voir les valeurs intermédiaires
-"""
-
 import requests
 
-#on obtient la page html au format texte
-page_html = requests.get("https://www.bdb.be/Productendiensten/Weerbericht/Onlineweerbericht/tabid/157/language/fr-BE/Default.aspx").text
+class Rgj(object):
+	"""Rgj pour Rayonnement global journalier"""
+	def __init__(self):
+		self.page_html = requests.get("https://www.bdb.be/Productendiensten/Weerbericht/Onlineweerbericht/tabid/157/language/fr-BE/Default.aspx").text
+		self.experimental_ls = self.page_html.split("Rayonnement")
+		self.experimental_ls_2 = self.experimental_ls[1].split('Direction du vent')
+		self.experimental_ls_3 = self.experimental_ls_2[0].split("nowrap>")
+		self.rayonnements_globaux_journaliers = []
+		self.experimental_ls_5 = []
+		self.dates = []
+		self.results = []
 
-"""
-on crée une liste avec 2 éléments: 
-1) Tout ce qui precede le mot "Rayonnement" 
-2) Tout ce qui suit le mot "Rayonnement"
-"""
-experimental_ls = page_html.split("Rayonnement")
+		for i in range(1, len(self.experimental_ls_3)):
+			self.rayonnements_globaux_journaliers.append(self.experimental_ls_3[i].split("</td>")[0])
 
+		self.rayonnements_globaux_journaliers.remove("(global)")
+		self.rayonnements_globaux_journaliers.pop()
 
-#idem que précédemment mais avec "Direction du vent"
-experimental_ls_2 = experimental_ls[1].split('Direction du vent')
+		for elem in self.page_html.split("<tr> <td class='weatherColCaption' valign='middle' align='center'  nowrap></td>")[1].split('</tr><tr>')[0].split("nowrap>"):
+			self.experimental_ls_5.append(elem)
 
-#idem avec "nowrap>"
-experimental_ls_3 = experimental_ls_2[0].split("nowrap>")
+		for i in range(1, len(self.experimental_ls_5)):
+			self.dates.append(self.experimental_ls_5[i].split('</th>')[0])
 
-#idem avec </td>
-rayonnements_globaux_journaliers = []
-for i in range(1, len(experimental_ls_3)):
-	rayonnements_globaux_journaliers.append(experimental_ls_3[i].split("</td>")[0])
+		for i in range(7):
+			self.results.append("[*] {} => {} [J/cm2] : {}".format(self.dates[i], self.rayonnements_globaux_journaliers[i], self.rayonnements_globaux_journaliers[i+7]))
 
-rayonnements_globaux_journaliers.remove("(global)")
-rayonnements_globaux_journaliers.pop()
+		#print("\n*** Valeurs obtenues en temps réel sur le site du Service Pédologique de Belgique ASBL : https://www.bdb.be ***")
+	def get_results(self):
+		return self.results
+	def init_message(self):
 
-#liste finale, contient tout ce qu'il faut
-#print(rayonnements_globaux_journaliers)
+		return """
+	 ____                                                         _
+	|  _ \ __ _ _   _  ___  _ __  _ __   ___ _ __ ___   ___ _ __ | |_
+	| |_) / _` | | | |/ _ \| '_ \| '_ \ / _ \ '_ ` _ \ / _ \ '_ \| __|
+	|  _ < (_| | |_| | (_) | | | | | | |  __/ | | | | |  __/ | | | |_
+	|_| \_\__,_|\__, |\___/|_| |_|_| |_|\___|_| |_| |_|\___|_| |_|\__|
+	            |___/
+	  ____ _       _           _
+	 / ___| | ___ | |__   __ _| |
+	| |  _| |/ _ \| '_ \ / _` | |
+	| |_| | | (_) | |_) | (_| | |
+	 \____|_|\___/|_.__/ \__,_|_|
 
-#les étapes qui suivent permettent d'obtenir les dates via la page HTML
-experimental_ls_5 = []
-for elem in page_html.split("<tr> <td class='weatherColCaption' valign='middle' align='center'  nowrap></td>")[1].split('</tr><tr>')[0].split("nowrap>"):
-	experimental_ls_5.append(elem)
+	     _                              _ _
+	    | | ___  _   _ _ __ _ __   __ _| (_) ___ _ __   _
+	 _  | |/ _ \| | | | '__| '_ \ / _` | | |/ _ \ '__| (_)
+	| |_| | (_) | |_| | |  | | | | (_| | | |  __/ |     _
+	 \___/ \___/ \__,_|_|  |_| |_|\__,_|_|_|\___|_|    (_)
 
-dates = []
-for i in range(1, len(experimental_ls_5)):
-	dates.append(experimental_ls_5[i].split('</th>')[0])
+			"""
 
-"""
-Voici les valeurs intermédiaires intéressantes:
-print(dates)
-print(rayonnements_globaux_journaliers)
-"""
-
-if __name__ == "__main__":
-	print("""
- ____                                                         _
-|  _ \ __ _ _   _  ___  _ __  _ __   ___ _ __ ___   ___ _ __ | |_
-| |_) / _` | | | |/ _ \| '_ \| '_ \ / _ \ '_ ` _ \ / _ \ '_ \| __|
-|  _ < (_| | |_| | (_) | | | | | | |  __/ | | | | |  __/ | | | |_
-|_| \_\__,_|\__, |\___/|_| |_|_| |_|\___|_| |_| |_|\___|_| |_|\__|
-            |___/
-  ____ _       _           _
- / ___| | ___ | |__   __ _| |
-| |  _| |/ _ \| '_ \ / _` | |
-| |_| | | (_) | |_) | (_| | |
- \____|_|\___/|_.__/ \__,_|_|
-
-     _                              _ _
-    | | ___  _   _ _ __ _ __   __ _| (_) ___ _ __   _
- _  | |/ _ \| | | | '__| '_ \ / _` | | |/ _ \ '__| (_)
-| |_| | (_) | |_| | |  | | | | (_| | | |  __/ |     _
- \___/ \___/ \__,_|_|  |_| |_|\__,_|_|_|\___|_|    (_)
-
-		""")
-	for i in range(7):
-		print("[*] {} => {} [J/cm2] : {}".format(dates[i], rayonnements_globaux_journaliers[i], rayonnements_globaux_journaliers[i+7]))
-
-	print("\n*** Valeurs obtenues en temps réel sur le site du Service Pédologique de Belgique ASBL : https://www.bdb.be ***")
+if __name__ == '__main__':
+	query = Rgj()
+	print(query.init_message())
+	
+	for elem in query.get_results():
+		print(elem)
